3eff0012c77ee35fb08b0e2d21738b620d4f86e4
TomasSand <38017189+TomasSand@users.noreply.github.com>
2020-04-16 12:59:19 +0200

    Update midiplayertest.js

--- midiplayertest.js
+++ midiplayertest.js
@@ -1,62 +1,40 @@
 
-var midiNotes = [];
+console.clear();
 var noteArray = [];
 var timeArray = [];
+var totaltime;
+var notex;
 var i=0;
 
 
-//Listen midikeyboard
-function ListenMidikeyboard(){
-    var msg=document.getElementById('msg');
-    var evnt=document.getElementById('evnt');
-        console.log(navigator.requestMIDIAccess);
-    if (navigator.requestMIDIAccess) {
-        console.log('navigator.requestMIDIAccess ok');
-        navigator.requestMIDIAccess().then(requestMIDIAccessSuccess, requestMIDIAccessFailure);
-    } else {
-        console.log('navigator.requestMIDIAccess undefined');
-        msg.innerHTML = 'navigator.requestMIDIAccess undefined';
-    }
-}
+navigator.requestMIDIAccess().then(access => {
+   const devices = access.inputs.values();
+    for (let device of devices)
+        device.onmidimessage = onMidiMessage;
+        document.getElementById('evnt').innerHTML=devices.onMidiMessage;
+}).catch(console.error);
 
 
-
-
-function requestMIDIAccessFailure(e) {
-    console.log('requestMIDIAccessFailure', e);
-}
+function onMidiMessage(message){
     
-    
-function logKeys(){
-    var s = 'Note:';
-    for (var i = 0; i < midiNotes.length; i++) {
-        s = s + ' ' + midiNotes[i].pitch;
-        
-    }
-    evnt.innerHTML = s;
-    
-}
-
-
-function midNoteOn(pitch, velocity,timeStamp) {
-    midiNoteOff(pitch);
-    //seleziona se velocity=0 {play} else null;
-    if (velocity>0){       
-        //start time count
-        noteArray[i] = pitch;
-        timeArray[i] = timeStamp/1000;
-        document.getElementById('noteArray').innerHTML=("Note:"+pitch);
-        document.getElementById('timeArray').innerHTML=("Time Stamps:"+(timeStamp/1000));
-    } 
-
-    if (velocity==0){
-    i++;
-    timeArray[i] = timeStamp/1000;
-    document.getElementById('timeArray').innerHTML=("Time Stamps:"+(timeStamp/1000));
-    i++;
-    document.getElementById('NoteNumber').innerHTML=("Numero di note suonate:"+i);
+    if (message.data[2]>0){
+      noteArray[i]=message.data[1];
+      pitch=message.data[1];
+      timeArray[i]=message.timeStamp/1000;
+      timeStamp=message.timeStamp;
+      document.getElementById('noteArray').innerHTML=("Note:"+pitch);
+      document.getElementById('timeArray').innerHTML=("Time Stamps:"+(timeStamp/1000));
+      
     }
     
+    if (message.data[2]==0){
+        i++;
+        timeArray[i]=message.timeStamp/1000;
+        timeStamp=message.timeStamp;
+        document.getElementById('timeArray').innerHTML=timeStamp;
+        i++;
+    }
+
     if (i>21){
         alert("Generazione Risposta...");
         i=0;
@@ -65,49 +43,5 @@ function midNoteOn(pitch, velocity,timeStamp) {
 }
 
 
-function midiNoteOff(pitch) {
-    for (var i = 0; i < midiNotes.length; i++) {
-        if (midiNotes[i].pitch == pitch) {
-            if (midiNotes[i].envelope) {
-                midiNotes[i].envelope.cancel();
-            }
-            midiNotes.splice(i, 1);
-            return;
-        }
-    }
-}
 
-
-function midiOnStateChange(event) {
-    console.log('midiOnStateChange', event);
-    msg.innerHTML = event.port.manufacturer + ' ' + event.port.name + ' ' + event.port.state;
-}
-function midiOnMIDImessage(event) {
-    var data = event.data;
-    var cmd = data[0] >> 4;
-    var channel = data[0] & 0xf;
-    var type = data[0] & 0xf0;
-    var pitch = data[1];
-    var velocity = data[2];
-    var timeStamp = event.timeStamp;
-    console.log(timeStamp);
-    switch (type) {
-    case 144:
-        midNoteOn(pitch, velocity,timeStamp);
-        logKeys();
-        break;
-    case 128:
-        midiNoteOff(pitch);
-        logKeys();
-        break;
-    }
-}
-function requestMIDIAccessSuccess(midi) {
-    var inputs = midi.inputs.values();
-    for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
-        console.log('midi input', input);
-        input.value.onmidimessage = midiOnMIDImessage;
-    }
-    midi.onstatechange = midiOnStateChange;
-}
 
